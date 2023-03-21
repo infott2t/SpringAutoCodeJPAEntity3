4509589cffa26d1e38249c2b8baf337eed77e99f
Hyeonil Choi <tt2t2am1118@naver.com>
2023-03-21 23:22:32 +0900

    userRole입력 설명 바꿈.

--- src/main/java/org/example/shopex/User.java
+++ src/main/java/org/example/shopex/User.java
@@ -19,6 +19,6 @@ public class User {
             System.out.println("Task, 유저권한 입력오류. \n 입력방법. userRole, 'admin', 'seller', 'guest', 'advance' 중 하나를 입력하세요.");
 
         }
-        return "Task, 유저 권한 입력 성공. userRole "+this+"\n User Role: " + userRole;
+        return "Task, 유저 권한 입력 성공. User, "+this+"\n Input User Role: " + userRole;
     }
 }
