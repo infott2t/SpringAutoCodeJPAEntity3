32bd615384f8027c8ba155a346afd07e803adb82
Hyeonil Choi <tt2t2am1118@naver.com>
2023-04-16 02:08:43 +0900

    Cmd라인 명령처리해봤습니다. 다시 만들었네요.
    전화상담,TDD개발. 프로그래밍 테스트부분을 CommandLine입력형식으로 만들었습니다.
    
    실행방법은, org.example.shopex.cmd.CommandSystem을 실행해주면 됨니다~.
    
    실제 서비스. 실무를 생각하면서 만들어 봤습니다. 전화상담의 경우, 회원정보를 확인해야도 하겠구요. a/s를 받을 수 있잖아요.
    
    전문자격증. 제가 아는 자격은, 개인정보취급사이네요. 관련 자격이상인 경우, 상담업무가 가능하겠죠~.
    
    한국에서는 CPPF 개인정보취급사. 자격증. 응시자격도 없구요. 저도 공부하고 있네요. 참조, 한국CPO포럼(https://www.cpoforum.or.kr/bbs/board.php?bo_table=certificate)
    
    시험공부할 때 사이트, https://www.cppf.or.kr/
    
    공부. 또 개발해보세요~.
    
    도움이 되었으면 좋겠네요~.
    
     감사합니다.

--- src/main/java/org/example/shopex/cmd/MenuTablingCmdSys.java
+++ src/main/java/org/example/shopex/cmd/MenuTablingCmdSys.java
@@ -9,22 +9,23 @@ public class MenuTablingCmdSys {
     public String[] menuStrsArr;
     public int menuInt;
 
-    public String adminId;
+    public String adviserName;
     public LocalDateTime loginTime;
     //public String pw; //암호화 필요.
     public String loginText;
 
     MenuTablingCmdSys(){
-        
-        adminId = null; //시스템 부팅시, 로그인 관리자값을 null로 만들어 준다.
+
+        adviserName = null; //시스템 부팅시, 로그인 관리자값을 null로 만들어 준다.
         loginText = null; //시스템 부팅시, 로그인 접속시간을 null로 만들어 준다.
 
         menuStrsArr = new String[10];
-        menuStrsArr[1] = "Task, CommandSystem 생성\n명령어 API 1 ---------------\n관리자 로그인 해주세요.\n Y/N\n";
+        menuStrsArr[1] = "Task, CommandSystem 생성\n명령어 API 1 ---------------\n상담 서비스, 로그인 해주세요.\n Y/N\n? ";
         //menuStrsArr[2] = "1.회원가입\n2.로그인\n3.로그아웃\n4.회원정보수정\n";
-        menuStrsArr[3] = "ID를 입력해주세요. Now time, 'admin'.\n";
-        menuStrsArr[4] = "암호를 입력해주세요. Now time, '1234'.\n";
-        menuStrsArr[5] = "1.회원가입\n2.로그인\n3.로그아웃\n4.회원정보수정\n";
+        menuStrsArr[3] = "상담사님의 이름을 입력해주세요. Now time, '홍길동'.\n? ";
+        menuStrsArr[4] = "자격인증번호를 입력해주세요. Now time, '1234567'.\n? ";
+        menuStrsArr[5] = "(개발관련 메뉴 TDD)\n1.회원가입\n2.로그인\n3.로그아웃\n4.회원정보수정\n--------------------------\n(전화상담업무)\n5.회원확인\n6.A/S관련 상담기록\n? ";
+
     }
 
 
@@ -61,19 +62,19 @@ public class MenuTablingCmdSys {
 
         if(menuInt==3){
             // findAll을 사용해서, 관리자 아이디들을 불러온다음, adminId의 값과 맞는지 비교해야됨.
-            if("admin".equals(str)){
-               
+            if("홍길동".equals(str)){
+                this.adviserName = "홍길동";
                 return 4;
             }else{
                 return 1;
             }
         }
         if(menuInt==4){
-            if("1234".equals(str)){
-                this.adminId = "admin";
+            if("1234567".equals(str)){
+
                 this.loginTime = LocalDateTime.now();
-                //암호가 일치했으므로, 로그인 시간을 기록. 해당 아이디에 접속시간 기록.
-                this.loginText = "Task-- 관리자 로그인:"+ adminId + "\n 접속 시간:"+ loginTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))+"\n"+"--------------------------\n";
+                //자격인증번호가 일치했으므로, 로그인 시간을 기록. 해당 아이디에 접속시간 기록.
+                this.loginText = "Task----------------------\n 상담사 : "+ adviserName + " 님 \n로그인  접속 시간:"+ loginTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))+"\n"+"--------------------------\n";
                 
                 return 5;
             }else{
@@ -92,10 +93,16 @@ public class MenuTablingCmdSys {
 
             }else if("4".equals(str)){
 
+            }else if("5".equals(str)){
+
+            }else if("6".equals(str)){
+
             }else{
                 return 5; // 값 입력이 1~4가 아닌 경우, 자기자신. menuStrsArr[5]값으로 다시 menuInt 5값을 불러온다. menuStrsArr[menuInt]로 되어있음.
             }
             */
+
+
 
         }
         return -1;
