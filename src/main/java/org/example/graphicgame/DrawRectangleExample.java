2418f491eba898f0aaf57f851daa318894ac2fa3
Hyeonil Choi <tt2t2am1118@naver.com>
2023-04-16 14:16:11 +0900

    드레그해서 사각형을 그리고, 3번 클릭하면, 사각형 그리기.
    package org.example.graphicgame.DrawRectangleExample을 실행하면 됨니다.
    
    만들어봤습니다. 해보세요. 재미있네요.
    
    좋은 하루되세요.
    
    저의 git, 봐 주셔서 감사합니다.

--- src/main/java/org/example/graphicgame/DrawRectangleExample.java
+++ src/main/java/org/example/graphicgame/DrawRectangleExample.java
@@ -12,16 +12,41 @@ public class DrawRectangleExample extends JFrame {
     private Point startPoint;
     private Point endPoint;
 
-    //private Rectangle[] objectArr;
+
+    private int clickCount; //3. 세번 클릭하면, 사각형 생성. 마우스 클릭 횟수.
+    private Point clickPoint;       // clickPointFirst, clickPointTwice, clickPointThird가 같으면, clickPoint에 x,y값 저장.
+    private Point clickPointFirst;
+    private Point clickPointTwice;
+    private Point clickPointThird;
+
+    private Color[] colorArr;
+
+    private Rectangle[] objectArr;
 
     //private int a;
 
     public DrawRectangleExample() {
-        setTitle("Draw Rectangle Example");
+        setTitle("Draw Rectangle Example. Thrid Click, same point. Make Rectangle.");
         setSize(500, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //objectArr = new Rectangle[1000];
-        //a=0;
+
+        //사각형 배열 초기화
+        objectArr = new Rectangle[1000];
+        for (int i = 0; i < objectArr.length; i++) {
+            objectArr[i] = new Rectangle(0,0,0,0);
+        }
+
+        //색상 배열 초기화
+        /*
+        colorArr = new Color[1000];
+        for (int i = 0; i < colorArr.length; i++) {
+            colorArr[i] = Color.blue;
+        }
+        */
+
+
+        //더블클릭 카운트 초기화
+        clickCount = 0;
 
         canvas = new JPanel() {
             @Override
@@ -36,12 +61,28 @@ public class DrawRectangleExample extends JFrame {
                     int height = Math.abs(startPoint.y - endPoint.y);
                     g.setColor(Color.WHITE);
                     g.drawRect(x, y, width, height);
-
-
-
-
                 }
+                if(clickPoint!=null){
+                    g.setColor(Color.BLUE);
+                    g.fillRect(clickPoint.x-25, clickPoint.y-25, 50,50);
 
+                    //클릭하면, 사각형 1개 저장하기.
+                    for (int i = 0; i < objectArr.length; i++) {
+                        if(objectArr[i].getX()==0 && objectArr[i].getY()==0){
+                            objectArr[i]= new Rectangle(clickPoint.x-25, clickPoint.y-25, 50,50);
+                            break;
+                        }
+                    }
+
+                    //저장된 사각형 불러오기.
+                    for (int i = 0; i < objectArr.length; i++) {
+                        if(objectArr[i].getX()!=0 && objectArr[i].getY()!=0){
+                            g.setColor(Color.BLUE);
+                            //new Rectangle((int) objectArr[i].getX(),(int)objectArr[i].getY(),objectArr[i].width, objectArr[i].height);
+                            g.fillRect((int) objectArr[i].getX(),(int)objectArr[i].getY(),objectArr[i].width, objectArr[i].height);
+                        }
+                    }
+                }
 
             }
         };
@@ -60,6 +101,34 @@ public class DrawRectangleExample extends JFrame {
                 startPoint = e.getPoint();
 
 
+
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                clickPointFirst = e.getPoint();
+                clickCount++;
+
+                if(clickCount==2){
+                    clickPointTwice = e.getPoint();
+                }
+
+                if(clickCount==3){
+                    clickPointThird = e.getPoint();
+
+                    if(clickPointFirst.equals(clickPointTwice)){
+                        if(clickPointTwice.equals(clickPointThird)){
+                            clickPoint = clickPointFirst;
+                        }else{
+                            clickPointFirst = null;
+                            clickPointTwice = null;
+                            clickPointThird = null;
+                        }
+                    }
+                    clickCount=0;
+                }
 
             }
 
